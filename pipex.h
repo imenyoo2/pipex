b7401b54f7f14b7825c5fcc140f9335e1c1d6fc0
Ayoub Ait El Aouad <ayait-el@e3r7p3.1337.ma>
2023-12-13 18:01:55 +0100

    added header

---
+++ pipex.h
@@ -0,0 +1,20 @@
+
+
+
+#ifndef PIPEX_H
+#define PIPEX_H
+
+# ifdef DEBUG
+#  define KNRM  "\x1B[0m"
+#  define KRED  "\x1B[31m"
+#  define KGRN  "\x1B[32m"
+#  define KYEL  "\x1B[33m"
+#  define KBLU  "\x1B[34m"
+#  define KMAG  "\x1B[35m"
+#  define KCYN  "\x1B[36m"
+#  define KWHT  "\x1B[37m"
+
+#  define LOG(x) fprintf(stderr, KRED); x; fprintf(stderr, KNRM); fflush(stderr)
+# endif
+
+#endif
